4efa92662063ae19ebfc7159b2650e39f98ee2cf
BoNana25 <bmeers@stcharlessd.org>
2014-10-07 08:34:43 -0500

    Changed the layout of guess_your_number design

--- guess_your_number.py
+++ guess_your_number.py
@@ -1,26 +1,34 @@
 # Guess your number
 #author: Bo Meers
+
 import random  
 
 print("\tWelcome to 'Guess Your Number'!")
 print("\nI want you to think of a number between 1 and 100.")
 print("I'll try to guess it in as few attempts as possible.\n")
 
-the_number = int(input("What's your number? "))
 tries = 1
-b = random.randint(1,100)
+low = 1
+high = 100
+b = random.randint(low,high)
 
-if the_number > 100:
-    the_number = int(input("I'm pretty sure i said 1 to 100... try again "))
+a = input("Is your number " + str(b) + " ? (higher/lower/yes) ")
 
-elif the_number < 1:
-    the_number = int(input("I'm pretty sure i said 1 to 100... try again "))
-                     
-while b != the_number:
-    b = random.randint(1,100)
-    tries += 1
-    
-print("\nI guessed it!  The number was", the_number)
+while b != a:
+
+    if a == "higher":
+        low = b
+        b = random.randint(low, high)
+        tries += 1
+        a = input("Is your number " + str(b) + " ? (higher/lower/yes) ")
+
+    elif a == "lower":
+        high = b
+        b = random.randint(low, high)
+        tries += 1
+        a = input("Is your number " + str(b) + " ? (higher/lower/yes) ")
+        
+print("\nI guessed it!  The number was", a)
 print("And it only took me", tries, "tries!")
   
 input("\nPress the enter key to exit.")
