51b9a3894d327f9c206edbbb664aaa6b2ecc975c
MCoyne01 <MCoyne@StCharlesSD.org>
2014-10-13 08:38:19 -0500

    Challenges

--- guess_my_ number.py
+++ guess_my_ number.py
@@ -17,14 +17,15 @@ guess = int(input("Take a guess: "))
 tries = 1
 
 # guessing loop
-while guess != the_number:
-    if guess > the_number:
-        print("Lower...")
-    else:
-        print("Higher...")
+while tries < 10:
+    while guess != the_number:
+        if guess > the_number:
+            print("Lower...")
+        else:
+            print("Higher...")
             
-    guess = int(input("Take a guess: "))
-    tries += 1
+        guess = int(input("Take a guess: "))
+        tries += 1
 
 print("You guessed it!  The number was", the_number)
 print("And it only took you", tries, "tries!\n")
--- guess_your_ number.py
+++ guess_your_ number.py
@@ -12,18 +12,26 @@ print("\nI want you to think of a number between 1 and 100.")
 print("I'll try to guess it in as few attempts as possible.\n")
 
 # set the initial values
-
-
-
+number = input("Enter a number to guess(1-100): ")
+guess = random.randint(1,100)
+x = 1
+y = 100
+tries = 0
 # guessing loop
-
-
-
-
-
-
-
-print("I guessed it!  The number was", the_number)
+while guess != number:
+    if guess < number:
+        print(guess)
+        print("My guess was lower.")
+        x = guess
+        guess = random.randint(x,y)
+        tries += 1
+    elif guess > number:
+        print(guess)
+        print("My guess was higher.")
+        y = guess
+        guess = random.randint(x,y)
+        tries += 1
+print("I guessed it!  The number was", number)
 print("And it only took me", tries, "tries!\n")
   
 input("\n\nPress the enter key to exit.")
